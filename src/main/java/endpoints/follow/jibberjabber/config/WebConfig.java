c7e5f8c6ff4beec85f315573a534357f0a1d10d2
tefiporacchia <stefania.poracchia@ing.austral.edu.ar>
2022-07-13 17:54:35 -0300

    now hoping will work

--- src/main/java/endpoints/follow/jibberjabber/config/WebConfig.java
+++ src/main/java/endpoints/follow/jibberjabber/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("DELETE");
+        registry.addMapping("/**").allowedMethods("DELETE","GET","POST");
     }
 }
